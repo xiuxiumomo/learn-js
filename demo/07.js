3617a10a13ba27a760405ed7212b1f7a72425af3
xiuxiumomo <2331396362@qq.com>
2021-03-15 18:06:27 +0800

    提交

--- demo/07.js
+++ demo/07.js
@@ -5,8 +5,7 @@ function Person(name, age) {
     return this.name;
   };
 }
-Person.prototype.sex = 'man'
+
 let p = new Person('jack',20)
-console.log(p.hasOwnProperty('name'))
-console.log(p.hasOwnProperty('sex'))
-console.log('sex' in p)
+
+console.log(Person.constructor)
---
+++ demo/08.js
@@ -0,0 +1,9 @@
+const factorial = function f(num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * f(num - 1);
+  }
+};
+
+console.log(factorial(3));
